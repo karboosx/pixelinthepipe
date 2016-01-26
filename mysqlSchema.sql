2099dc41217fd793f1f7ec3766da3cc99b4fef9f
Michał Karbowiak <karbo243@gmail.com>
2016-01-27 00:27:16 +0100

    mysql schema

---
+++ mysqlSchema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `levels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
