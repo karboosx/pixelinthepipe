bf4c358d5d6a26861742c008a3a0921162d3a9cd
Michał Karbowiak <karbo243@gmail.com>
2016-01-26 21:55:41 +0100

    extra stages list

---
+++ extra.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Pixel in the Pipe</title>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-ui.js"></script>
+
+    <script src="js/item.js"></script>
+    <script src="js/levels.js"></script>
+
+    <link rel="stylesheet" href="css/intro.css">
+    <link rel="stylesheet" href="css/stages.css">
+
+    <link href='http://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<h1><a href="index.html">Pixel in the Pipe!</a></h1>
+
+<div id="viewport-big" class="no-background">
+    <div id="screen">
+	    <?php
+
+	    include "connect.php";
+
+	    if (isset($_GET['page']))
+	        $page = (int) $_GET['page'];
+	    else
+		    $page = 0;
+
+	    $itemsPerPage = 10;
+
+	    $file = $pdo->prepare('select `name` FROM levels order by id limit :startID,:limit;');
+
+	    $startID = $page*$itemsPerPage;
+
+	    $file->bindParam(':startID', $startID, PDO::PARAM_INT);
+	    $file->bindParam(':limit', $itemsPerPage, PDO::PARAM_INT);
+	    $file->execute();
+
+	    foreach ($file->fetchAll() as $level){
+		    ?>
+		    <a href="extra_<?php echo $level['name'] ?>.html" class="level">
+			    <h3><?php echo $level['name'] ?></h3>
+		    </a>
+	        <?php
+	    }
+
+	    ?>
+    </div>
+</div>
+<a class="back" href="index.html">Back to menu</a>
+
+<?php echo include "analytics.php"; ?>
+</body>
+</html>
