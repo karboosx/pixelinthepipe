e096b31be05376b5244a33f556e19455bcd0d263
Michał Karbowiak <karbo243@gmail.com>
2016-01-27 18:20:45 +0100

    mysql schema update

--- mysqlSchema.sql
+++ mysqlSchema.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS `levels` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`filename` varchar(255) NOT NULL,
+`password` varchar(255) NOT NULL,
+`name` varchar(100) NOT NULL,
+`subname` varchar(100) NOT NULL,
+`difficult` enum('supereasy','easy','normal','hard') NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE KEY `name` (`filename`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
